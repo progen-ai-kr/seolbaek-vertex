353e92db9cc5660024e73a05f69bd662233bd59f
박수훈 <86775077+PrakSuHun@users.noreply.github.com>
2026-08-25 08:44:52 +0900

    팀별 사이트 선택 기능 추가

--- brand-config.js
+++ brand-config.js
@@ -3,4 +3,8 @@ window.BRAND_SITE = Object.freeze({
   name: "설백",
   adminUrl: "https://seolbaek-admin.progen-web.workers.dev/admin",
   siteUrl: "https://seolbaek-vertex.progen-web.workers.dev",
+  sites: [
+    { team: "텐서플로우", url: "https://seolbaek-tensorflow.progen-web.workers.dev" },
+    { team: "Vertex", url: "https://seolbaek-vertex.progen-web.workers.dev" },
+  ],
 });
